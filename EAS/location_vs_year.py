6317cc662309ea1e27a633f601d82f5fccd9375b
Aranda Rizki Soedjono <arsoedjono@gmail.com>
2016-05-25 19:15:13 +0700

    loc vs year code fix

--- EAS/location_vs_year.py
+++ EAS/location_vs_year.py
@@ -1,48 +1,45 @@
 __author__ = 'Aranda Rizki Soedjono'
 
+# libraries
 from pyspark import SparkConf, SparkContext
 from operator import add
 
+# spark configuration
 conf = SparkConf().setMaster( "local" ).setAppName( "LocationVsYear" )
 sc = SparkContext( conf = conf )
 
+# map function to get year & location from dataset
 def parseLine( line ):
   fields = line.split( ';' )
   year = fields[1].encode( 'utf-8' ).strip()
   locations = fields[2].encode( 'utf-8' ).strip().replace( "\"", "" )
   return ( year, locations )
 
-def reduceValue( x, y ):
-  print "x = " + str( x )
-  return ( x, y )
-  # if x[0] == y[0]:
-  #   return x[1] + y[1]
-  # else:
-  #   return x[1]
-
+# read file
 lines = sc.textFile( "dataset.dat" )
-rdd = lines.map( parseLine )
+# get each line
+rdd = lines.map( parseLine ).filter( lambda x: x[1] != "" )
 
-distinctByYear = rdd.distinct()
-# locationYearMapValue = distinctByYear.mapValues( lambda x: ( x, 1 ) )
-# locationYearMapValue.reduceByKey( lambda x, y: x )
-
-locationYearCount = distinctByYear.map( lambda x: ( ( x[0], x[1] ), 1 ) )
+# map location and year pair to count similar
+locationYearCount = rdd.map( lambda x: ( ( x[0], x[1] ), 1 ) )
 locationYearSum = locationYearCount.reduceByKey( add )
 
+# re-map, year as key
 locationByYear = locationYearSum.map( lambda x: ( x[0][0], ( x[0][1], x[1] ) ) )
-# locationByYear = locationYearSum.map( lambda x: ( x[0][0], x[1] ) )
-# locationYear = locationByYear.reduceByKey( lambda x, y: max( x, y ) )
+# group per year and sort
 locationGroupByYear = locationByYear.groupByKey().sortByKey()
 
+# collect result
 results = locationGroupByYear.collect()
 
+# print result
 length = 0
 for result in results:
   max_count = ["", 0]
   for locs in result[1]:
     if locs[1] > max_count[1]:
       max_count = locs
-  print result[0] + ": " + max_count[0]
+  res = result[0] + ": " + max_count[0]
+  print res + " (" + str( max_count[1] ) + ")" if max_count[0] != "" else res
   length = length + 1
 print "Jumlah data: " + str( length )
